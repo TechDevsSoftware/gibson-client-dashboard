07fae697286afa78fb0dc830578a363fe3337f3d
Steve <stevekent55@gmail.com>
2018-12-24 19:57:41 +0000

    Updated the portal root

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiRoot: "https://techdevs-accounts.azurewebsites.net",
   clientKey: "techdevs",
-  customerPortalRoot: "https://techdevs-custportal.netlify.com"
+  customerPortalRoot: "https://app.techedvs.net"
 };
